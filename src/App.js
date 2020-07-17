4e72e492d29761a0bac8eea08a8121008eb146c9
SEBASTIAN <sebastian@MacBook-Pro-de-SEBASTIAN.local>
2020-07-17 14:56:51 -0500

    error

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import casa from './Home';
+import Cuarto from './Cuarto';
 const App= () => <BrowserRouter>
   <Switch>
     <Route path="/" exact component= {casa}></Route>
-    
+    <Route path="/extern" exact component= {Cuarto}></Route>
+    <Route component={() => <div>404</div>}></Route>
   </Switch>
 </BrowserRouter>
   
---
+++ src/Cuarto.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+const HeaderStyled = styled.div
+
+
+function Cuarto() {
+    return(
+        <HeaderStyled>
+            Header
+        </HeaderStyled>
+    )
+}
+ export default Cuarto;
